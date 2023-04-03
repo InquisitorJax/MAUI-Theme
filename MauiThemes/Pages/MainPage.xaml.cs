4b96b53a72e4443951404da0d1c86b3c5c7d6a31
Malcolm Jack <inquisitorjax@gmail.com>
2023-04-04 00:19:59 +0200

    add some collectionview span runtime updates

--- MauiThemes/Pages/MainPage.xaml.cs
+++ MauiThemes/Pages/MainPage.xaml.cs
@@ -17,5 +17,33 @@ public partial class MainPage : ContentPage
 		base.OnAppearing();
 		_ = _viewModel.InitializeAsync();
 	}
+
+	protected override void OnSizeAllocated(double width, double height)
+	{
+		base.OnSizeAllocated(width, height);
+
+		int gridSpan = 1;
+		if (width > 1000)
+		{
+			gridSpan = 3;
+		}
+		else if (width > 500)
+		{
+			gridSpan = 2;
+		}
+
+		// changing span not working when windows resizing
+		// https://github.com/dotnet/maui/pull/13137
+		if (_layout.Span != gridSpan)
+		{
+			// crash on iOS
+			MainThread.BeginInvokeOnMainThread(() =>
+			{
+				_layout.Span = gridSpan;
+			});
+		}
+
+	}
+
 }
 
