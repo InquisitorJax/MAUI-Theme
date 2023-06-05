437dec568ca7c39217b649ca48af296b6d78c53b
Malcolm Jack <inquisitorjax@gmail.com>
2023-06-05 23:45:27 +0200

    code-behind theme files = fixed?

---
+++ MauiThemes/Resources/Styles/DarkTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiThemes.Resources.Styles;
+
+public partial class DarkTheme : ResourceDictionary
+{
+	public DarkTheme()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiThemes/Resources/Styles/LightTheme.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiThemes.Resources.Styles;
+
+public partial class LightTheme : ResourceDictionary
+{
+	public LightTheme()
+	{
+		InitializeComponent();
+	}
+}
